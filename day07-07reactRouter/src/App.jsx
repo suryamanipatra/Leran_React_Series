8e7202ef1ac1b0be48032835639059b965bdc7ce
suryamanipatra <suryamanipatra114@gmail.com>
2024-06-30 15:49:20 +0530

    Your commit message here

---
+++ day07-07reactRouter/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div className='text-3xl text-white'>Github Follower</div>
+      
+    </>
+  )
+}
+
+export default App
